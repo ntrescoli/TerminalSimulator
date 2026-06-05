42ada39232aad36c7a123f3e044492ade382c1fa
nicolas <nicolastrescoli@gmail.com>
2026-06-05 14:48:07 +0200

    prueba

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 // Punto de entrada del paquete NPM
+import './style.css';
+
 export { TSTerminal } from './TSTerminal'; // Para proyectos Vanilla/JS
 export { ReactTerminal } from './ReactTerminal'; // Para proyectos React
